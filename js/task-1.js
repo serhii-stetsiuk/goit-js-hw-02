70b09cfc0f065d7aaf76ee86fd8503dd77b7a266
SERHII STETSIUK <skystets@gmail.com>
2025-06-03 12:05:07 +0200

    Task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,18 @@
 'use strict';
-function makeTransaction(quantity, pricePerDroid) {
-	return `You ordered ${quantity} droids worth ${pricePerDroid * quantity} credits!`;
+
+function makeTransaction(quantity, pricePerDroid, customerCredits){
+	const sum = quantity * pricePerDroid;
+	let x;
+	if (sum > customerCredits) {
+		x = "Insufficient funds!";
+	} else {
+		x = `You ordered ${quantity} droids worth ${sum} credits!`;
+	}
+	return x;
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
