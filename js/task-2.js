8fe68ea1c908f00c621fb0eeb97edec5552a051a
SERHII STETSIUK <skystets@gmail.com>
2025-06-05 10:03:26 +0200

    Finish hw 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,21 @@
 'use strict';
+
+'use strict';
+
+function formatMessage(message, maxLength) {
+	const z = message.slice(0, maxLength);
+	let x; 
+	if (maxLength >= message.length) {
+		x = message;
+	} else {
+		x = z + "...";	
+	}
+	return x;
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function getShippingCost(country) {
+	const myCountry = country;
+	let price;
+	let x;
+	switch (myCountry) {
+		case "China": price = 100;
+			x = `Shipping to ${myCountry} will cost ${price} credits`;
+			break;
+		
+		case "Chile": price = 250;
+			x = `Shipping to ${myCountry} will cost ${price} credits`;
+			break;
+		case "Australia": price = 170;
+			x = `Shipping to ${myCountry} will cost ${price} credits`;
+			break;
+		case "Jamaica": price = 120;
+			x = `Shipping to ${myCountry} will cost ${price} credits`;
+			break;
+		default: x = "Sorry, there is no delivery to your country";
+		
+	}
+	return x;
+}
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
